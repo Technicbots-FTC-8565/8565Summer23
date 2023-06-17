001865ec160ba07e5f53476fbff43923e3a26769
ryan <tyryanosaur@gmail.com>
2023-06-17 10:28:57 -0500

    add test class and bump minSdkVersion to 24 to support some specific features

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Test {
-
+    public static void main(String[] args) {
+        System.out.println("This is a test");
+        System.exit(0);
+    }
 }
