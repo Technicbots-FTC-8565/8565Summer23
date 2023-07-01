093781d363b946d67c1b0a74655849ba308a2ca1
ryan <tyryanosaur@gmail.com>
2023-06-30 20:50:53 -0500

    relocalizer complete except for F, h, and H

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/localization/Relocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/localization/Relocalizer.java
@@ -7,6 +7,9 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.ejml.data.DMatrixRMaj;
 import org.ejml.simple.SimpleMatrix;
 
+import java.util.List;
+import java.util.Objects;
+
 @Config
 public class Relocalizer extends ExtendedKalmanFilter {
     // Diagonal matrix elements for process noise cov matrix Q
@@ -19,24 +22,40 @@ public class Relocalizer extends ExtendedKalmanFilter {
     public static double R2 = 1;
     public static double R3 = 1;
     private ThreeWheelTrackingLocalizer odometry;
+    private Rev2mDistanceDriver distanceDriver;
 
-    public Relocalizer(Pose2d initialPose, HardwareMap hardwareMap) {
-        super(new DMatrixRMaj(), new DMatrixRMaj(), new DMatrixRMaj());
-        this.odometry = new ThreeWheelTrackingLocalizer(hardwareMap);
+    public Relocalizer(HardwareMap hardwareMap, List<Integer> lastTrackingEncPositions, List<Integer> lastTrackingEncVels, Pose2d initialPose) {
+        // construct diagonal Q and R noise cov matrices
+        super(new DMatrixRMaj(new double[][] {
+                new double[] { Q1, 0, 0 },
+                new double[] { 0, Q2, 0 },
+                new double[] { 0, 0, Q3 }
+        }), new DMatrixRMaj(new double[][] {
+                new double[] { R1, 0, 0 },
+                new double[] { 0, R2, 0 },
+                new double[] { 0, 0, R3 }
+        }), poseToMatrix(initialPose));
+        this.odometry = new ThreeWheelTrackingLocalizer(hardwareMap, lastTrackingEncPositions, lastTrackingEncVels);
+        this.distanceDriver = new Rev2mDistanceDriver(hardwareMap);
     }
 
     public static DMatrixRMaj poseToMatrix(Pose2d pose) {
         return new DMatrixRMaj(new double[] { pose.getX(), pose.getY(), pose.getHeading() });
     }
+
+    public static Pose2d matrixToPose(DMatrixRMaj matrix) {
+        return new Pose2d(matrix.data[0], matrix.data[1], matrix.data[2]);
+    }
     /**
      * describes how the state evolves
-     *
+     * just going to use the regular odometry prediction for the state update
      * @param u input vector
      * @return delta vector for state estimate based on u and current state estimate
      */
     @Override
     public SimpleMatrix f(DMatrixRMaj u) {
-        return null;
+        odometry.update();
+        return new SimpleMatrix(poseToMatrix(Objects.requireNonNull(odometry.getPoseVelocity())));
     }
 
     /**
@@ -67,6 +86,6 @@ public class Relocalizer extends ExtendedKalmanFilter {
     }
 
     public void update() {
-
+        super.iterate(new DMatrixRMaj((double[][]) Objects.requireNonNull(odometry.getWheelVelocities().toArray())), new DMatrixRMaj((double[][]) distanceDriver.getDistances().toArray()));
     }
 }
