d8a555ae3e7bcd829126b1aa27dc86587b219fa9
ryan <tyryanosaur@gmail.com>
2023-07-01 19:24:15 -0500

    add in the relocalizationtest opmode name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/localization/RelocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/localization/RelocalizationTest.java
@@ -3,9 +3,11 @@ package org.firstinspires.ftc.teamcode.localization;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.localization.drive.MecanumDriveCancellablev3;
 
+@TeleOp(name = "Relocalization Test", group = "Testing")
 public class RelocalizationTest extends OpMode {
     private MecanumDriveCancellablev3 chassis;
     @Override
