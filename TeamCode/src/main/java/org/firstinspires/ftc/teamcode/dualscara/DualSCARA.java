ea8ca4162547eb10fc920491bc8fb3291749ca78
ryan <tyryanosaur@gmail.com>
2023-08-01 10:49:23 -0500

    finish coding and stuff for dual scara + test opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dualscara/DualSCARA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dualscara/DualSCARA.java
@@ -2,17 +2,26 @@ package org.firstinspires.ftc.teamcode.dualscara;
 
 import static org.firstinspires.ftc.teamcode.dualscara.MathEx.square;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
 public class DualSCARA {
 	private boolean shouldIk;
 	private final double L0, A1, A2, B1, B2;
-	private double T1, T2;
-	private double workingMode1, workingMode2, fkMode;
-	private double x, y, xCache, yCache;
+	public double T1, T2, x, y;
+	private double workingMode1, workingMode2, fkMode, xCache, yCache;
+	private Servo left, right;
+	public static double INIT_ANGLE = 0.4;
+	public static double SERVO_ROM = Math.toRadians(270); // Range of motion of the servo, in radians
+	public static double SERVO_LEFT_DEFAULT = 0.5; // To get an angle of 0 on L2
+	public static double SERVO_RIGHT_DEFAULT = 0.5; // To get an angle of 0 on L1
 	
-	public DualSCARA(double L0, double A1, double A2, double B1, double B2) {
+	public DualSCARA(HardwareMap hardwareMap, double L0, double A1, double A2, double B1, double B2) {
 		this.L0 = L0;
-		this.T1 = Math.PI * 0.6;
-		this.T2 = Math.PI * 0.4;
+		this.T1 = Math.PI * (1d - INIT_ANGLE);
+		this.T2 = Math.PI * INIT_ANGLE;
 		this.A1 = A1;
 		this.A2 = A2;
 		this.B1 = B1;
@@ -24,14 +33,22 @@ public class DualSCARA {
 		this.workingMode1 = 1;
 		this.workingMode2 = 1;
 		this.fkMode = 1;
+		this.left = hardwareMap.servo.get("leftSCARA");
+		this.right = hardwareMap.servo.get("rightSCARA");
 		this.disableIk();
-		this.forwardKinematics();
+		this.update(); // fk to set up x and y to the right values
 		this.enableIk();
 	}
 
 	public void update() {
 		if (this.shouldIk) this.inverseKinematics();
 		else this.forwardKinematics();
+		this.move();
+	}
+
+	public void move() {
+		this.left.setPosition(SERVO_LEFT_DEFAULT + -(this.T1 - Math.PI) / SERVO_ROM);
+		this.right.setPosition(SERVO_RIGHT_DEFAULT + -this.T2 / SERVO_ROM);
 	}
 
 	public void forwardKinematics() {
@@ -45,10 +62,8 @@ public class DualSCARA {
 		double v3 = 1 + square(v1);
 		double v4 = 2 * (v1 * v2 - v1 * x_R1 - y_R1);
 		double v5 = square(v2) + square(x_R1) - 2 * v2 * x_R1 + square(y_R1) - square(this.B1);
-		double y_p = (-v4 + this.fkMode * Math.sqrt(square(v4) - 4 * v3 * v5)) / (2 * v3);
-		double x_p = v1 * y_p + v2;
-		this.x = x_p;
-		this.y = y_p;
+		this.y = (-v4 + this.fkMode * Math.sqrt(square(v4) - 4 * v3 * v5)) / (2 * v3);
+		this.x = v1 * this.y + v2;
 	}
 
 	public void disableIk() {
@@ -56,20 +71,28 @@ public class DualSCARA {
 	}
 
 	public void enableIk() {
-		this.shouldIk = true;
 		this.x = xCache;
 		this.y = yCache;
+		this.shouldIk = true;
+	}
+
+	public boolean shouldIk() {
+		return shouldIk;
+	}
+
+	public void toggleIk() {
+		this.shouldIk = !this.shouldIk;
 	}
 
 	public void flipFk() {
 		this.fkMode *= -1;
 	}
 
-	public void flipLeftWorkingMode() {
+	public void flipLeft() {
 		this.workingMode1 *= -1;
 	}
 
-	public void flipRightWorkingMode() {
+	public void flipRight() {
 		this.workingMode2 *= -1;
 	}
 
@@ -91,6 +114,14 @@ public class DualSCARA {
 		double theta_2 = alpha_2 + beta_2;
 		this.T1 = theta_1;
 		this.T2 = Math.PI - theta_2;
+	}
+
+	public double getServoLeft() {
+		return this.left.getPosition();
+	}
+
+	public double getServoRight() {
+		return this.right.getPosition();
 	}
 
 	/**
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dualscara/DualSCARATest.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.dualscara;
+
+
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Config
+@TeleOp(name = "Dual SCARA Test", group = "Testing")
+public class DualSCARATest extends OpMode {
+    private DualSCARA scara;
+    private GamepadEx gpad1;
+    public static double SENS = 0.00001;
+    public static double L0 = mmToInches(240);
+    public static double A1 = mmToInches(108);
+    public static double A2 = A1;
+    public static double B1 = mmToInches(180);
+    public static double B2 = B1;
+
+    public static double mmToInches(double mm) {
+        return mm * 0.03937007874;
+    }
+
+    @Override
+    public void init() {
+        scara = new DualSCARA(hardwareMap, L0, A1, A2, B1, B2);
+        gpad1 = new GamepadEx(gamepad1);
+    }
+
+    @Override
+    public void loop() {
+        gpad1.readButtons();
+        if (gpad1.wasJustPressed(GamepadKeys.Button.A)) scara.toggleIk();
+        else if (gpad1.wasJustPressed(GamepadKeys.Button.X)) scara.flipLeft();
+        else if (gpad1.wasJustPressed(GamepadKeys.Button.B)) scara.flipRight();
+        else if (gpad1.wasJustPressed(GamepadKeys.Button.Y)) scara.flipFk();
+        if (scara.shouldIk()) {
+            double dx = SENS * gpad1.getLeftX();
+            double dy = SENS * gpad1.getRightY();
+            if (scara.canMove(scara.x + dx, scara.y + dy)) {
+                scara.x += dx;
+                scara.y += dy;
+            }
+        }
+        else {
+            double d1 = gpad1.getLeftX() * SENS / 40;
+            double d2 = gpad1.getRightX() * SENS / 40;
+            scara.T1 += d1;
+            scara.T2 += d2;
+        }
+
+        scara.update();
+        telemetry.addData("T1", scara.T1);
+        telemetry.addData("T2", scara.T2);
+        telemetry.addData("Servo Left", scara.getServoLeft());
+        telemetry.addData("Servo Right", scara.getServoRight());
+    }
+}
