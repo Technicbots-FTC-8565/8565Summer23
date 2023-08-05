8ae64432ffb3117bfc001dd89f3e2b096ac09ebc
ryan <tyryanosaur@gmail.com>
2023-08-05 14:01:08 -0500

    fix loader for assets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dualscara/util/Loader.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dualscara/util/Loader.java
@@ -1,17 +1,12 @@
 package org.firstinspires.ftc.teamcode.dualscara.util;
 
-import android.util.Log;
-
 import com.acmerobotics.dashboard.config.Config;
-
-import org.firstinspires.ftc.teamcode.dualscara.util.Point;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.FileSystems;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,15 +17,14 @@ public class Loader {
     public static double ORIG_HEIGHT = 631.7796610169491 * SCALING_FACTOR;
     /**
      * Load a CSV to parsed points
-     * @param filename CSV location relative to org.firstinspires.ftc.teamcode.dualscara.res
+     * @param filename CSV location relative to project's "Assets" folder
      * @param canvasWidth Width of the IRL canvas in IRL units
      * @param canvasHeight Height of the IRL canvas in IRL units
      * @param scaling Factor to scale the image by
      * @return The parsed points from the CSV file
      */
-    public static List<List<Point>> load(String filename, double canvasWidth, double canvasHeight, double scaling) {
-        Log.d("8565 file debug", FileSystems.getDefault().getPath(".").toAbsolutePath().toString());
-        try (BufferedReader br = new BufferedReader(new FileReader("sdcard/FIRST/DualSCARA/res/".concat(filename)))) {
+    public static List<List<Point>> load(OpMode opMode, String filename, double canvasWidth, double canvasHeight, double scaling) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(opMode.hardwareMap.appContext.getAssets().open(filename)))) {
             double scalingFactor = Math.min(canvasWidth / ORIG_WIDTH, canvasHeight / ORIG_HEIGHT) * scaling;
             List<List<Point>> ret = new ArrayList<>();
             List<Point> pointBuffer = new ArrayList<>();
